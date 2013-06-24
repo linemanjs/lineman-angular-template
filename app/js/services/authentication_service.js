88e4c435de4dc2ae20cb29067652b771e80d5773
David Mosher <davidmosher@gmail.com>
2013-06-24 10:11:00 -0500

    Update authentication_service.js
    clarified that the API endpoints are stubbed

--- app/js/services/authentication_service.js
+++ app/js/services/authentication_service.js
@@ -1,5 +1,5 @@
 angular.module("app").factory('AuthenticationService', function($http) {
-  // these routes are configured in config/server.js
+  // these routes map to stubbed API endpoints in config/server.js
   return {
     login: function(credentials) {
       return $http.post('/login', credentials);
