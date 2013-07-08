f88cdf060f49b7766aac5cea15a3cb93de795481
David Mosher <davidmosher@gmail.com>
2013-07-08 11:37:54 -0600

    Swaps watch config targets for concat w/ sourcemap
    - this is needed to avoid linemans default watch targets from re-running
    the concat tasks, and instead run our concat_sourcemap tasks

--- config/application.js
+++ config/application.js
@@ -71,11 +71,41 @@ module.exports = require(process.env['LINEMAN_MAIN']).config.extend('application
     }
   },
 
-  // configures grunt-watch-nospawn to listen for changes to, and recompile angular templates
+  // configures grunt-watch-nospawn to listen for changes to
+  // and recompile angular templates, also swaps lineman default
+  // watch target concat with concat_sourcemap
   watch: {
     ngtemplates: {
       files: "app/templates/**/*.html",
       tasks: ["ngtemplates", "concat_sourcemap:js"]
+    },
+    js: {
+      files: ["<%= files.js.vendor %>", "<%= files.js.app %>"],
+      tasks: ["concat_sourcemap:js"]
+    },
+    coffee: {
+      files: "<%= files.coffee.app %>",
+      tasks: ["coffee", "concat_sourcemap:js"]
+    },
+    jsSpecs: {
+      files: ["<%= files.js.specHelpers %>", "<%= files.js.spec %>"],
+      tasks: ["concat_sourcemap:spec"]
+    },
+    coffeeSpecs: {
+      files: ["<%= files.coffee.specHelpers %>", "<%= files.coffee.spec %>"],
+      tasks: ["coffee", "concat_sourcemap:spec"]
+    },
+    css: {
+      files: ["<%= files.css.vendor %>", "<%= files.css.app %>"],
+      tasks: ["concat_sourcemap:css"]
+    },
+    less: {
+      files: ["<%= files.less.vendor %>", "<%= files.less.app %>"],
+      tasks: ["less", "concat_sourcemap:css"]
+    },
+    sass: {
+      files: ["<%= files.sass.vendor %>", "<%= files.sass.app %>"],
+      tasks: ["sass", "concat_sourcemap:css"]
     }
   }
 
