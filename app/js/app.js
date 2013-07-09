6d4fb8c1f91c9410d2b98ca147f894a8dd851c0a
David Mosher <davidmosher@gmail.com>
2013-07-09 10:14:18 -0600

    Example: added $resource example

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-angular.module("app", []).run(function($rootScope) {
+angular.module("app", ["ngResource"]).run(function($rootScope) {
   // adds some basic utilities to the $rootScope for debugging purposes
   $rootScope.log = function(thing) {
     console.log(thing);
--- app/js/controllers/books_controller.js
+++ app/js/controllers/books_controller.js
@@ -1,3 +1,11 @@
-angular.module("app").controller("BooksController", function ($scope, BookService) {
-  $scope.books = BookService.getBooks();
+// with $resource
+angular.module("app").controller("BooksController", function ($scope, BookResource) {
+  // because the stubbed endpoint returns an array of results, .query() is used
+  // if the endpoint returned an object, you would use .get()
+  $scope.books = BookResource.query();
 });
+
+// with $http
+// angular.module("app").controller("BooksController", function ($scope, BookService) {
+//   $scope.books = BookService.getBooks();
+// });
