d948fed538a5418102abc1d074bbd2a24d6c06f3
David Mosher <davidmosher@gmail.com>
2013-07-11 11:03:13 -0600

    Makes the config setup to spinup webdriver

--- config/spec-e2e.js
+++ config/spec-e2e.js
@@ -1,10 +1,10 @@
 exports.config = {
 
-  // seleniumServerJar: '/usr/local/opt/selenium-server-standalone/selenium-server-standalone-2.33.0.jar',
-  // seleniumPort: 4444,
-  // seleniumArgs: [],
-  // chromeDriver: '/usr/local/opt/chromedriver/bin/chromedriver',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumServerJar: '/usr/local/opt/selenium-server-standalone/selenium-server-standalone-2.33.0.jar',
+  seleniumPort: 4444,
+  seleniumArgs: [],
+  chromeDriver: '/usr/local/opt/chromedriver/bin/chromedriver',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
 
   capabilities: {
     'browserName': 'chrome'
