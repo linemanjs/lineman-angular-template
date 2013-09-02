5dc428dbdd6f477a48da374c89fb09790dabc791
David Mosher <davidmosher@gmail.com>
2013-09-02 15:29:27 -0600

    Reverts to chrome for e2e specs
    If you have issues with Chrome, `brew upgrade chromedriver`

--- config/spec-e2e.js
+++ config/spec-e2e.js
@@ -42,7 +42,7 @@ exports.config = {
   // and
   // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
   capabilities: {
-    'browserName': 'firefox'
+    'browserName': 'chrome'
   },
 
   // A base URL for your application under test. Calls to protractor.get()
