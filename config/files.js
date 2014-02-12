8f6f491956517af8bcce0c27b81c63e79f520a5d
Justin Searls <searls@gmail.com>
2014-02-12 15:03:15 -0500

    Bring config back into line with the archetype.

--- config/files.js
+++ config/files.js
@@ -1,33 +1,30 @@
-/* Exports an object that defines
- *  all of the paths & globs that the project
- *  is concerned with.
+/* Exports a function which returns an object that overrides the default &
+ *   plugin file patterns (used widely through the app configuration)
  *
- * The "configure" task will require this file and
- *  then re-initialize the grunt config such that
- *  directives like <config:files.js.app> will work
- *  regardless of the point you're at in the build
- *  lifecycle.
+ * To see the default definitions for Lineman's file paths and globs, see:
  *
- * You can find the parent object in: node_modules/lineman/config/files.coffee
+ *   - https://github.com/linemanjs/lineman/blob/master/config/files.coffee
  */
+module.exports = function(lineman) {
+  //Override file patterns here
+  return {
+    js: {
+      vendor: [
+        "vendor/js/angular.js",
+        "vendor/js/**/*.js"
+      ],
+      app: [
+        "app/js/app.js",
+        "app/js/**/*.js"
+      ]
+    },
 
-module.exports = require(process.env['LINEMAN_MAIN']).config.extend('files', {
-  js: {
-    vendor: [
-      "vendor/js/angular.js",
-      "vendor/js/**/*.js"
-    ],
-    app: [
-      "app/js/app.js",
-      "app/js/**/*.js"
-    ]
-  },
-
-  less: {
-    compile: {
-      options: {
-        paths: ["vendor/css/normalize.css", "vendor/css/**/*.css", "app/css/**/*.less"]
+    less: {
+      compile: {
+        options: {
+          paths: ["vendor/css/normalize.css", "vendor/css/**/*.css", "app/css/**/*.less"]
+        }
       }
     }
-  }
-});
+  };
+};
