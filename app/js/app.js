8f5badd9d39fbb94a5a951866890f3c325265647
David Mosher <davidmosher@gmail.com>
2014-04-28 10:44:40 -0400

    ngRoute -> ui.route - also note, $routeProvider becomes a combination of $stateProvider and $urlRouterProvider - ng-view becomes ui-view

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-angular.module("app", ["ngResource", "ngRoute"]).run(function($rootScope) {
+angular.module("app", ["ngResource", "ui.router"]).run(function($rootScope) {
   // adds some basic utilities to the $rootScope for debugging purposes
   $rootScope.log = function(thing) {
     console.log(thing);
