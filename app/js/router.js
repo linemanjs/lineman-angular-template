95cd03ae80b29b02fa99779c4db91b72c6337f19
Shaun  Hubbard <shaun.hubbard@ascentis.com>
2015-01-12 11:01:28 -0500

    changes made per @davemo's comments

--- app/js/router.js
+++ app/js/router.js
@@ -1,6 +1,6 @@
 angular.module("app").config(function($routeProvider, $locationProvider) {
 
-  $locationProvider.html5Mode({enabled:true, requireBase: false});
+  $locationProvider.html5Mode({enabled:true});
 
   $routeProvider.when('/login', {
     templateUrl: 'login.html',
